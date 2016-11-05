8077e73da483e0e7c9c3ca671fd1404ac3d231bc
kedarnadkarny <kedar.nadkarny@gmail.com>
2016-11-05 16:57:50 -0500

    DBAccess folder

---
+++ app/src/main/java/edu/uta/utarepairingservices/TableInfo/UserTableInfo.java
@@ -0,0 +1,18 @@
+package edu.uta.utarepairingservices.TableInfo;
+
+import android.provider.BaseColumns;
+
+public class UserTableInfo {
+
+    public UserTableInfo() {
+
+    }
+
+    public static abstract class TableInfo implements BaseColumns {
+        public static final String USER_NAME = "user_name";
+        public static final String USER_PASS = "user_pass";
+        public static final String DATABASE_NAME = "user_info";
+        public static final String TABLE_NAME = "reg_info";
+
+    }
+}
