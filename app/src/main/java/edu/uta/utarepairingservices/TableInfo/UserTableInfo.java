55ccb28e4be1dd87e1af3cea1b112d90f5ee8d70
kedarnadkarny <kedar.nadkarny@gmail.com>
2016-11-05 17:01:03 -0500

    TableInfo Folder

--- app/src/main/java/edu/uta/utarepairingservices/TableInfo/UserTableInfo.java
+++ app/src/main/java/edu/uta/utarepairingservices/TableInfo/UserTableInfo.java
@@ -9,10 +9,10 @@ public class UserTableInfo {
     }
 
     public static abstract class TableInfo implements BaseColumns {
-        public static final String USER_NAME = "user_name";
-        public static final String USER_PASS = "user_pass";
-        public static final String DATABASE_NAME = "user_info";
-        public static final String TABLE_NAME = "reg_info";
+        public static final String FIRST_NAME = "first_name";
+        public static final String LAST_NAME = "last_name";
+        public static final String DATABASE_NAME = "utadb";
+        public static final String TABLE_NAME = "customer";
 
     }
 }
