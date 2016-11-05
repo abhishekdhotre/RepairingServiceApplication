9d7823e407f7b4092d4c5187471915099da10fba
kedarnadkarny <kedar.nadkarny@gmail.com>
2016-11-05 17:05:44 -0500

    AdminHomeActivity

--- app/src/main/java/edu/uta/utarepairingservices/AdminHomeActivity.java
+++ app/src/main/java/edu/uta/utarepairingservices/AdminHomeActivity.java
@@ -2,8 +2,10 @@ package edu.uta.utarepairingservices;
 
 // Home Page for our Admin
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminHomeActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class AdminHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+    }
+
+    public void openAddUserActivity(View v) {
+        Intent intent = new Intent(this, AddUserActivity.class);
+        startActivity(intent);
     }
 }
