03d0a0e7ee45a5eba7304ec4458b23f771b34495
kedarnadkarny <kedar.nadkarny@gmail.com>
2016-11-25 17:01:10 -0600

    Entire

--- app/src/main/java/edu/uta/utarepairingservices/CustomerHomeActivity.java
+++ app/src/main/java/edu/uta/utarepairingservices/CustomerHomeActivity.java
@@ -4,12 +4,23 @@ package edu.uta.utarepairingservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class CustomerHomeActivity extends AppCompatActivity {
+
+    private static String ID = "";
+    TextView tv14;
+
+    public static void setID(String ID) {
+        CustomerHomeActivity.ID = ID;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_home);
+        tv14 = (TextView) findViewById(R.id.textView14);
+        tv14.setText(ID);
     }
 }
